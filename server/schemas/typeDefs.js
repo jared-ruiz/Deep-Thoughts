7066f67692c54a5999bc828c7a2f84826c5c191a
Jared Ruiz <jared707@gmail.com>
2022-06-27 17:32:15 -0500

    Added starter code and graphQL middle ware intergration with express.

---
+++ server/schemas/typeDefs.js
@@ -0,0 +1,13 @@
+//import gql tagged template function
+
+const { gql } = require('apollo-server-express');
+
+//create our typeDefs
+const typeDefs = gql`
+    type Query {
+        helloWorld: String
+    }
+`;
+
+//export typeDefs
+module.exports = typeDefs;
