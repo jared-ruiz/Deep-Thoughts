0026f4f777d1cceaa670fe34610f650d9ef556a8
Jared Ruiz <jared707@gmail.com>
2022-06-27 19:41:36 -0500

    Added all queries and resolvers for users and thoughts.

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -1,7 +1,33 @@
+const { User, Thought } = require('../models');
+
+//resolvers can accept 4 arguments (parent, args, context, info)
 const resolvers = {
     Query: {
-        helloWorld: () => {
-            return 'Hello World!';
+        //get all thoughts by username
+        thoughts: async (parent, { username }) => {
+            const params = username ? { username } : {};
+            return Thought.find(params).sort({ createdAt: -1 })
+        },
+
+        //get thought by id
+        thought: async (parent, { _id }) => {
+            return Thought.findOne({_id});
+        },
+
+        //get all users
+        users: async () => {
+            return User.find()
+            .select('-__v -password')
+            .populate('friends')
+            .populate('thoughts') 
+        },
+        
+        //get user by id
+        user: async (parent, { username }) => {
+            return User.findOne({ username })
+            .select('-__v -password')
+            .populate('friends')
+            .populate('thoughts')
         }
     }
 }
