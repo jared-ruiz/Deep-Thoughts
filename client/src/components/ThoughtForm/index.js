4ef8dad29d4396938dc02a087f2e9f0e73be6f50
Jared Ruiz <jared707@gmail.com>
2022-07-01 23:16:24 -0500

    Added reation body creation and cache refreshing upon submission of a new thought or reaction.

--- client/src/components/ThoughtForm/index.js
+++ client/src/components/ThoughtForm/index.js
@@ -1,4 +1,7 @@
 import React, { useState } from 'react';
+import { useMutation } from '@apollo/client';
+import { ADD_THOUGHT } from '../../utils/mutations';
+import { QUERY_THOUGHTS, QUERY_ME } from '../../utils/queries';
 
 const ThoughtForm = () => {
 
@@ -14,14 +17,55 @@ const ThoughtForm = () => {
 
     const handleSubmit = async event => {
         event.preventDefault();
-        setText('');
-        setCharacterCount(0);
+
+        try {
+            //add thought to database
+            await addThought({
+                variables: { thoughtText }
+            })
+
+            //clear for value
+            setText('');
+            setCharacterCount(0);
+        }
+        catch (e) {
+            console.error(e);
+        }
     }
+
+    const [addThought, {error}] = useMutation(ADD_THOUGHT, {
+        update(cache, { data: { addThought} }) {
+            //read what's currently in the cache
+
+            //could potentially not exist yet, so wrap in a try/catch
+            try {
+                //update me array's cache
+                const { me } = cache.readQuery({ query: QUERY_ME });
+                cache.writeQuery({
+                    query: QUERY_ME,
+                    data: { me: { ...me, thoughts: [...me.thoughts, addThought] } },
+                })
+            }
+            catch (e) {
+                console.warn('First thought insertion by user!')
+            }
+
+            //update thoughtArray's cache
+            const { thoughts } = cache.readQuery({ query: QUERY_THOUGHTS })
+
+            //prepend the newest thought to the front of the array
+            cache.writeQuery({
+                query: QUERY_THOUGHTS,
+                data: { thoughts: [addThought, ...thoughts] }
+            })
+        }
+    });
 
     return(
         <div>
-            <p className={`m-0 ${characterCount === 280 ? 'text-error' : ''}`}>
+            <p className={`m-0 ${characterCount === 280 || error ? 'text-error' : ''}`}>
                 Character Count: {characterCount}/280
+                {error && <span className='ml-2'>Something went wrong...</span>}
             </p>
             <form 
                 className='flex-row justify-center justify-space-between-md align-stretch'
